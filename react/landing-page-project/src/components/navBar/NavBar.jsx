acef4b168d73bebfbea09ccaac598b31a82f36f0
Manos Koufadakis <manos.koufadakis@uicentric.com>
2025-10-03 17:30:56 +0100

    [REACT] Practice landing page (using vite and tailwindcss)

---
+++ react/landing-page-project/src/components/navBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <div className='w-full h-[80px] bg-white border-b'>
+            <div className='max-w-[1200px] m-auto w-full h-full flex justify-between items-center'>
+
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
